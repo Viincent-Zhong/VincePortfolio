9664bd0dd1369aad40f7134503501b5200ca4979
Viincent-Zhong <vincent.zhong@epitech.eu>
2024-09-17 21:27:37 +0200

    [ADD] Style for project cards

--- src/app/[lang]/(home)/projects.tsx
+++ src/app/[lang]/(home)/projects.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import Image from "next/image";
 import { useState } from "react";
 
 interface Props {
@@ -34,13 +35,29 @@ export default function Projects({ dictionary } : Props) {
                 ))}
             </div>
             {/* Projects */}
-            <div className="mt-10 grid grid-cols-1 lg:grid-cols-3 w-full px-24 lg:px-96 gap-6">
+            <div className="mt-10 grid grid-cols-1 lg:grid-cols-3 w-full px-24 lg:px-52 gap-10">
                 {(projects.find(([category, _]) => category === selectedCategory)?.at(1) as Project[]).map((project, index) => {return (
-                    <div key={index} className={`order-${index + 1} col-span-1 w-full h-96 flex flex-row justify-center bg-transparent neon-border hover:zoom ease-in-out duration-300`}>
-                        {project.name}
-                        <p className="whitespace-pre">
-                            {dictionary[project.description]}
-                        </p>
+                    <div key={index} className={`order-${index + 1} col-span-1 w-full h-[60rem] flex flex-row justify-center bg-transparent neon-border hover:zoom ease-in-out duration-300`}>
+                        {/* Image */}
+                        <div className="w-full h-full flex flex-col overflow-y-auto">
+                            <div className="relative w-full h-[20rem]">
+                                <Image src={project.image} alt={project.name} fill={true} sizes="w-full h-full" className="p-6" />
+                            </div>
+                            {/* Name */}
+                            <h1 className="text-center text-4xl lg:text-5xl text-white font-bold break-words">
+                                {project.name}
+                            </h1>
+                            {/* Text */}
+                            <p className="mt-10 whitespace-pre px-8 text-sm lg:text-xl text-white font-mono h-[30rem] overflow-y-auto">
+                                {dictionary[project.description]}
+                            </p>
+                            {/* Link */}
+                            <div className="w-full flex flex-row justify-center">
+                                <div className="w-40 h-14 bg-gradient-to-r from-indigo-900 to-indigo-950 flex flex-row justify-center items-center cursor-pointer rounded-md" onClick={() => {window.open(project.link, "_blank")}}>
+                                    <h1 className="text-white text-xl text-center">{dictionary.redirection}</h1>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 )})}
             </div>
